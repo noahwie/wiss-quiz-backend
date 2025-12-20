5de3677a70d610441b9bb17ccbc1955cb3308cf0
Noah WIeschmann <124290589+noahwie@users.noreply.github.com>
2025-12-20 10:45:51 +0100

    feat: changed AppUser, added Enum Role

--- src/main/java/com/wiss/quizbackend/entity/AppUser.java
+++ src/main/java/com/wiss/quizbackend/entity/AppUser.java
@@ -1,11 +1,8 @@
 package com.wiss.quizbackend.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.UniqueElements;
 
 @Entity
 @Table(name = "app_users")
---
+++ src/main/java/com/wiss/quizbackend/entity/Role.java
@@ -0,0 +1,6 @@
+package com.wiss.quizbackend.entity;
+
+public enum Role {
+    ADMIN,
+    PLAYER
+}
