9a5c0443104fab91c3f0862453fb1c44df3ce783
Noah WIeschmann <124290589+noahwie@users.noreply.github.com>
2026-01-17 11:51:32 +0100

    feat: added custom functions to repository

--- src/main/java/com/wiss/quizbackend/repository/GameSessionRepository.java
+++ src/main/java/com/wiss/quizbackend/repository/GameSessionRepository.java
@@ -3,6 +3,14 @@ package com.wiss.quizbackend.repository;
 import com.wiss.quizbackend.entity.GameSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameSessionRepository  extends JpaRepository<GameSession, Long> {
+import java.util.List;
 
+public interface GameSessionRepository  extends JpaRepository<GameSession, Long> {
+    List<GameSession> findByUserId(Long userId);
+
+    List<GameSession> findByCategory(Long category);
+
+    List<GameSession> findByUserIdOrderByPlayedAtDesc(Long userId);
+
+    List<GameSession> findByUserIdAndCategory(Long userId, Long category);
 }
